bf1333cd13bf4b8845d279801cca1ef44f7a8e0b
Jamy Golden <code@jamygolden.com>
2024-02-15 12:30:44 +0100

    Add `init` operation test for config.default_scheme

--- tests/cli_init_subcommand_tests.rs
+++ tests/cli_init_subcommand_tests.rs
@@ -1,8 +1,11 @@
 mod common;
 
-use crate::common::{cleanup, COMMAND_NAME, REPO_NAME};
-use anyhow::Result;
-use std::path::Path;
+use crate::common::{cleanup, read_file_to_string, write_to_file, COMMAND_NAME, REPO_NAME};
+use anyhow::{anyhow, Result};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 #[test]
 fn test_cli_init_subcommand_without_setup() -> Result<()> {
@@ -64,6 +67,52 @@ fn test_cli_init_subcommand_with_setup() -> Result<()> {
         stdout.is_empty(),
         "stdout does not contain the expected output"
     );
+
+    cleanup(config_path)?;
+    Ok(())
+}
+
+#[test]
+fn test_cli_init_subcommand_with_config_default_scheme() -> Result<()> {
+    // -------
+    // Arrange
+    // -------
+    let config_path = Path::new("test_cli_init_subcommand_with_config_default_scheme");
+    let system_data_path: PathBuf =
+        dirs::data_dir().ok_or_else(|| anyhow!("Error getting data directory"))?;
+    let data_dir = system_data_path.join(format!("tinted-theming/{}", REPO_NAME));
+    let config_file_path = config_path.join("config.toml");
+    let scheme_name = "base16-mocha";
+    let command = format!(
+        "{} init --config=\"{}\"",
+        COMMAND_NAME,
+        config_path.display()
+    );
+    let config_content = format!("default-scheme = \"{}\"", scheme_name);
+    let command_vec = shell_words::split(command.as_str()).map_err(anyhow::Error::new)?;
+    cleanup(config_path)?;
+    fs::create_dir(config_path)?;
+    write_to_file(&config_file_path, config_content.as_str())?;
+
+    // // ---
+    // // Act
+    // // ---
+    common::run_setup_command(config_path)?;
+    let (stdout, stderr) = common::run_command(command_vec).unwrap();
+
+    // // ------
+    // // Assert
+    // // ------
+    let expected_scheme_name = read_file_to_string(&data_dir.join("current_scheme"))?;
+    assert!(
+        stdout.is_empty(),
+        "stdout does not contain the expected output"
+    );
+    assert!(
+        stderr.is_empty(),
+        "stdout does not contain the expected output"
+    );
+    assert_eq!(scheme_name, expected_scheme_name);
 
     cleanup(config_path)?;
     Ok(())
