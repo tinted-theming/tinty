eedb63b631aa60f5f196cc5ca7930916e411909d
Jamy Golden <code@jamygolden.com>
2024-02-19 01:07:55 +0100

    Make cli help data more consistent by removing trailing fullstops

--- src/cli.rs
+++ src/cli.rs
@@ -11,7 +11,7 @@ pub fn build_cli() -> Command {
         .arg(
             Arg::new("config")
                 .short('c')
-                .help(format!("Optional path to the {} config.toml file.", REPO_NAME))
+                .help(format!("Optional path to the {} config.toml file", REPO_NAME))
                 .value_name("FILE")
                 .long("config")
                 .global(true)
@@ -21,14 +21,14 @@ pub fn build_cli() -> Command {
             Command::new("current").about("Prints the last scheme name applied")
         )
         .subcommand(
-            Command::new("info").about(format!("Shows scheme colors for all schemes matching <scheme_system>-<scheme_name> (Eg: {} info base16-mocha).", REPO_NAME)).arg(
+            Command::new("info").about(format!("Shows scheme colors for all schemes matching <scheme_system>-<scheme_name> (Eg: {} info base16-mocha)", REPO_NAME)).arg(
                 Arg::new("scheme_name")
                     .help("The scheme you want to get information about")
                     .required(false),
             )
         )
         .subcommand(
-            Command::new("init").about("Initializes with the exising config. Used to Initialize exising theme for when your shell starts up.")
+            Command::new("init").about("Initializes with the exising config. Used to Initialize exising theme for when your shell starts up")
         )
         .subcommand(Command::new("list").about("Lists available schemes"))
         .subcommand(
@@ -39,9 +39,9 @@ pub fn build_cli() -> Command {
             ),
         )
         .subcommand(
-            Command::new("install").about(format!("Install the environment needed for {}.", REPO_NAME))
+            Command::new("install").about(format!("Install the environment needed for {}", REPO_NAME))
         )
         .subcommand(
-            Command::new("update").about("Update to the latest themes.")
+            Command::new("update").about("Update to the latest themes")
         )
 }
