029cb4ff888e6ff26ed5ea30a1218730665a959c
Bez Hermoso <bezalelhermoso@gmail.com>
2025-01-17 18:14:43 -0800

    test: tests for `list --json`

--- tests/cli_list_subcommand_tests.rs
+++ tests/cli_list_subcommand_tests.rs
@@ -2,9 +2,14 @@ mod utils;
 
 use crate::utils::REPO_NAME;
 use anyhow::Result;
+use serde::Deserialize;
+use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
+use tinted_builder::{SchemeSystem, SchemeVariant};
 use utils::setup;
+
+const SCHEME_COUNT: usize = 287;
 
 #[test]
 fn test_cli_list_subcommand_without_setup() -> Result<()> {
@@ -141,6 +146,110 @@ fn test_cli_list_subcommand_with_custom() -> Result<()> {
             "stdout does not contain the expected output"
         );
     }
+
+    cleanup()?;
+    Ok(())
+}
+
+#[derive(Clone, Deserialize, PartialEq)]
+struct TestSchemeEntry {
+    pub id: String,
+    pub name: String,
+    pub author: String,
+    pub system: SchemeSystem,
+    pub variant: SchemeVariant,
+    pub slug: String,
+    pub palette: HashMap<String, TestColorOut>,
+    pub lightness: Option<TestLightness>,
+}
+
+impl std::ops::Deref for TestSchemeEntry {
+    type Target = SchemeSystem;
+
+    fn deref(&self) -> &Self::Target {
+        &self.system
+    }
+}
+
+impl std::fmt::Debug for TestSchemeEntry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("TestSchemeEntry")
+            .field("id", &self.id)
+            .field("name", &self.name)
+            .field("author", &self.author)
+            .field("system", &self.system)
+            .field("variant", &self.variant)
+            .field("slug", &self.slug)
+            .field("palette", &self.palette)
+            .field("lightness", &self.lightness)
+            .finish()
+    }
+}
+
+#[derive(Clone, Deserialize, Debug, PartialEq)]
+struct TestColorOut {
+    pub hex_str: String,
+    pub hex: (String, String, String),
+    pub rgb: (u8, u8, u8),
+    pub dec: (f32, f32, f32),
+}
+
+#[derive(Clone, Deserialize, Debug, PartialEq)]
+struct TestLightness {
+    pub foreground: f32,
+    pub background: f32,
+}
+
+#[test]
+fn test_cli_list_subcommand_as_json_with_setup() -> Result<()> {
+    // -------
+    // Arrange
+    // -------
+    let (config_path, data_path, command_vec, cleanup) =
+        setup("test_cli_list_subcommand_as_json_with_setup", "list --json")?;
+
+    // ---
+    // Act
+    // ---
+    utils::run_install_command(&config_path, &data_path)?;
+    let (stdout, _) = utils::run_command(command_vec).unwrap();
+
+    let results: Vec<TestSchemeEntry> = serde_json::from_str(&stdout).unwrap();
+
+    assert!(
+        results.len() == SCHEME_COUNT,
+        "expected JSON to contain {} entries, found {}",
+        SCHEME_COUNT,
+        results.len()
+    );
+
+    let entry_map: HashMap<String, TestSchemeEntry> =
+        results.into_iter().map(|e| (e.id.clone(), e)).collect();
+
+    let github = entry_map.get("base16-github").unwrap().clone();
+    let gruvbox = entry_map
+        .get("base16-gruvbox-material-dark-hard")
+        .unwrap()
+        .clone();
+
+    let github_json = fs::read_to_string(Path::new("fixtures/github.json"))?;
+    let gruvbox_json = fs::read_to_string(Path::new("fixtures/gruvbox-material-dark-hard.json"))?;
+
+    let expected_github: TestSchemeEntry = serde_json::from_str(&github_json).unwrap();
+    let expected_gruvbox: TestSchemeEntry = serde_json::from_str(&gruvbox_json).unwrap();
+
+    assert!(
+        expected_github == github,
+        "{:?} does not match {:?}",
+        expected_github,
+        github
+    );
+    assert!(
+        expected_gruvbox == gruvbox,
+        "{:?} does not match {:?}",
+        expected_gruvbox,
+        gruvbox
+    );
 
     cleanup()?;
     Ok(())
